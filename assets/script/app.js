849589727b07b42bc1c523fe134008c0c9000cc8
Evan Kennedy <evanckennedy@protonmail.com>
2024-03-28 15:42:56 -0500

    create basic styling and structure

---
+++ assets/script/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// This app requires a server to handle import statements
+// and CORS issues
+import * as utils from './utils.js';
+import Contact from './Contact.js';
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Organizer                                            */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+utils.print('Welcome');
