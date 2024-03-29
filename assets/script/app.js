a325fd6518d701da7e1ff15e3cdd449b0d61b06e
Evan Kennedy <evanckennedy@protonmail.com>
2024-03-28 19:03:05 -0500

    create basic functionality

--- assets/script/app.js
+++ assets/script/app.js
@@ -6,7 +6,71 @@ import * as utils from './utils.js';
 import Contact from './Contact.js';
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Organizer                                            */
+/*  Selectors, Delcarartions                             */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+const input = utils.select('.input');
+const contactsContainer = utils.select('.contact-grid-container');
+const addButton = utils.select('.add-button');
+const savedContacts = utils.select('.saved-contacts span')
+let contactArray = [];
+let contactCount = 0;
 
-utils.print('Welcome');
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Add contact                                          */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+function getInputArr() {
+  const inputArr = input.value.split(',').map(ele => ele.trim());
+  return inputArr;
+}
+
+/* pass above as your argument */
+function createContactObj(inputArr) {
+  let newContact = new Contact(...inputArr);
+  contactArray.push(newContact);
+  contactCount++;
+  return newContact
+}
+
+function updateContactCount() {
+  savedContacts.textContent = contactCount;
+}
+
+function createContact() {
+  if (contactCount > 9) {
+    return;
+  }
+
+  let newContact = createContactObj(getInputArr());
+
+  let contactContainer = document.createElement('div');
+  const contactContent = `
+  <div>
+    <p>Name: ${newContact.name}</p>
+    <p>City: ${newContact.city}</p>
+    <p>Email: ${newContact.email}</p>
+  </div>
+  `; 
+  contactContainer.innerHTML = contactContent;
+  contactsContainer.prepend(contactContainer);
+
+  updateContactCount();
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Validation                                           */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* Return true if input array has 3 elements */
+function hasThreeElements(inputArr) {
+  return inputArr.length === 3;
+}
+
+/* returns true if email is valid */
+function hasValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Event Listners                                       */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+utils.listen('click', addButton, createContact)
